37ef451f617fa451961d59fe33b7b4798e9c66fa
小林聖 <kobayashikiyoshi@KobaTakaMacBook-Air.local>
2020-11-12 16:52:22 +0900

    recipigram用のrepository作成

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+
+Refile.secret_key = 'bd16e9d871db8cb842fcad2919459c725079e0f8bec5d0345fcb5b86fe45e50c09c68ded5b558f0a9e587fc47b157e7ba8d0c1f0dd24dc8c5eb841b3321c5cf2'
