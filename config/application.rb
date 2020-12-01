a223461101249188fa2a5945e56ad5d2fc8a0cff
小林聖 <kobayashikiyoshi@KobaTakaMacBook-Air.local>
2020-12-01 13:12:02 +0900

    commit

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module App2
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
-  
+  config.assets.initialize_on_precompile = false
+
 end
